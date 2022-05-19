bdf91f7a9fd9a8dff9c652c9dec27604b837bf56
svenrdz <svenbmr@gmail.com>
2022-05-19 17:53:46 +0200

    initial commit

---
+++ esgpull/__init__.py
@@ -0,0 +1,5 @@
+MAJOR = 4
+MINOR = 0
+PATCH = 0
+
+__version__ = f"{MAJOR}.{MINOR}.{PATCH}"
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from esgpull import __version__
+
+setup(
+    name="esgpull",
+    packages=find_packages(),
+    include_package_data=True,
+    version=__version__,
+    entry_points={"console_scripts": ["esgpull=esgpull.cli:main"]},
+    # url='https://github.com/Prodiguer/synda',
+    description="ESGF Data transfer Program",
+    long_description="This program download files from the Earth System Grid "
+    "Federation (ESGF) archive using command line.",
+    # zip_safe=False,
+    license="Public",
+    platforms="Linux",
+    maintainer="Sven Rodriguez",
+    maintainer_email="srodriguez@ipsl.fr",
+)
