67a7a3f99e0ec7c647e175ebc4ca5b8b0cc4eb0b
svenrdz <svenbmr@gmail.com>
2022-11-04 17:13:28 +0100

    feat(cli): use tui.UI & logger in facet

--- esgpull/cli/facet.py
+++ esgpull/cli/facet.py
@@ -1,21 +1,24 @@
 import click
-import rich
 
 from esgpull import Esgpull
-from esgpull.cli.decorators import args
+from esgpull.cli.decorators import args, opts
 from esgpull.db.models import Param
+from esgpull.tui import Verbosity
 
 
 @click.command()
 @args.key
+@opts.verbosity
 def facet(
     key: str | None,
+    verbosity: Verbosity,
 ):
-    esg = Esgpull()
-    if key is None:
-        with esg.db.select(Param.name) as stmt:
-            params = stmt.distinct().scalars
-    else:
-        with esg.db.select(Param.value) as stmt:
-            params = stmt.where(Param.name == key).scalars
-    rich.print(sorted(params))
+    esg = Esgpull.with_verbosity(verbosity)
+    with esg.ui.logging("facet"):
+        if key is None:
+            with esg.db.select(Param.name) as stmt:
+                params = stmt.distinct().scalars
+        else:
+            with esg.db.select(Param.value) as stmt:
+                params = stmt.where(Param.name == key).scalars
+        esg.ui.print(sorted(params))
