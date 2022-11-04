f93fbc538df248938f178d4842a4d5d263b5bae0
svenrdz <svenbmr@gmail.com>
2022-11-04 17:07:40 +0100

    feat(cli): use tui.UI & logger in install

--- esgpull/cli/install.py
+++ esgpull/cli/install.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 import click
-import rich
-from click.exceptions import Exit
+from click.exceptions import Abort, Exit
 
 from esgpull import Esgpull
 from esgpull.cli.decorators import args, opts
 from esgpull.cli.utils import load_facets
 from esgpull.db.models import File
+from esgpull.tui import Verbosity
 from esgpull.utils import format_size
 
 
@@ -21,6 +21,7 @@ from esgpull.utils import format_size
 @opts.data_node
 @opts.replica
 @opts.selection_file
+@opts.verbosity
 @args.facets
 def install(
     facets: list[str],
@@ -33,9 +34,10 @@ def install(
     replica: bool | None,
     selection_file: str | None,
     since: str | None,
+    verbosity: Verbosity,
 ) -> None:
-    esg = Esgpull()
-    with esg.context() as ctx:
+    esg = Esgpull.with_verbosity(verbosity)
+    with esg.context() as ctx, esg.ui.logging("install", onraise=Abort):
         ctx.distrib = distrib
         ctx.latest = latest
         ctx.since = since
@@ -45,7 +47,7 @@ def install(
             raise click.UsageError("No search terms provided.")
         hits = ctx.file_hits
         nb_files = sum(hits)
-        rich.print(f"Found {nb_files} files.")
+        esg.ui.print(f"Found {nb_files} files.")
         if nb_files > 500 and distrib:
             # Enable better distrib
             ctx.index_nodes = esg.fetch_index_nodes()
@@ -53,7 +55,7 @@ def install(
             queries = ctx._build_queries_search(
                 hits, file=True, max_results=nb_files, offset=0
             )
-            rich.print(queries)
+            esg.ui.print(queries)
             raise Exit(0)
         if not force and nb_files > 5000:
             nb_req = nb_files // 50
@@ -67,8 +69,8 @@ def install(
         )
         files = [File.from_dict(result) for result in results]
         total_size = sum([file.size for file in files])
-        rich.print(f"Total size: {format_size(total_size)}")
+        esg.ui.print(f"Total size: {format_size(total_size)}")
         if not force:
             click.confirm("Continue?", default=True, abort=True)
         installed = esg.install(*files)
-        rich.print(f"Installed {len(installed)} new files.")
+        esg.ui.print(f"Installed {len(installed)} new files.")
