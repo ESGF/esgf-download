c670c1cf1b1049a8772a95e69716e0352b4c2762
svenrdz <svenbmr@gmail.com>
2023-03-20 16:49:33 +0100

    remove dead code

--- esgpull/models/query.py
+++ esgpull/models/query.py
@@ -133,7 +133,6 @@ class QueryDict(TypedDict):
     options: NotRequired[MutableMapping[str, bool | None]]
     selection: NotRequired[MutableMapping[str, FacetValues]]
     files: NotRequired[list[FileDict]]
-    # children: NotRequired[list[QueryDict]]
 
 
 class Query(Base):
