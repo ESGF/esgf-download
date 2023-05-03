77b5151ce6e570bbaabf8bdf316cf3ea31f01d0e
svenrdz <svenbmr@gmail.com>
2023-05-03 16:45:17 +0200

    test(config): test rename search->api + fixer

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,8 @@
+import pytest
+import tomlkit
+
 from esgpull.config import Config
+from esgpull.exceptions import BadConfigError
 
 
 def test_from_path(root):
@@ -7,3 +11,31 @@ def test_from_path(root):
     assert config.paths.data.parent == root
     assert config.paths.db.parent == root
     assert config.paths.tmp.parent == root
+
+
+def test_deprecated_search_api(root, config_path):
+    root.mkdir(parents=True)
+    with open(config_path, "w") as f:
+        tomlkit.dump({"search": {"http_timeout": 1234}}, f)
+    try:
+        config = Config.load(root)
+        assert config.api.http_timeout == 1234
+    except Exception:
+        raise
+    finally:
+        config_path.unlink()
+
+
+def test_deprecated_search_api_error(root, config_path):
+    root.mkdir(parents=True)
+    with open(config_path, "w") as f:
+        tomlkit.dump(
+            {"search": {"http_timeout": 1234}, "api": {"max_concurrent": 1}}, f
+        )
+    try:
+        with pytest.raises(BadConfigError):
+            Config.load(root)
+    except Exception:
+        raise
+    finally:
+        config_path.unlink()
