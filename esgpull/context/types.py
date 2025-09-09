72c5d186dda886fbbcdf18e059603710bf2cd446
Sven Rodriguez <svenbmr@gmail.com>
2025-09-09 18:09:01 +0200

    rewrite IndexNode to use ApiBackend enum

--- esgpull/context/types.py
+++ esgpull/context/types.py
@@ -2,25 +2,38 @@ from dataclasses import dataclass
 from typing import TypeAlias
 from urllib.parse import urlparse
 
+from esgpull.models import ApiBackend
+
 HintsDict: TypeAlias = dict[str, dict[str, int]]
 
 
 @dataclass
 class IndexNode:
     value: str
+    backend: ApiBackend
 
     def is_bridge(self) -> bool:
-        return "esgf-1-5-bridge" in self.value
+        match self.backend:
+            case ApiBackend.solr:
+                return "esgf-1-5-bridge" in self.value
+            case ApiBackend.stac:
+                return False
 
     @property
     def url(self) -> str:
         parsed = urlparse(self.value)
         result: str
-        match (parsed.scheme, parsed.netloc, parsed.path, self.is_bridge()):
-            case ("", "", path, True):
-                result = "https://" + parsed.path
-            case ("", "", path, False):
-                result = "https://" + parsed.path + "/esg-search/search"
+        match (
+            self.backend,
+            parsed.scheme,
+            parsed.netloc,
+            parsed.path,
+            self.is_bridge(),
+        ):
+            case (ApiBackend.solr, "", "", path, False):
+                result = f"https://{path}/esg-search/search"
+            case (_, "", "", path, _):
+                result = f"https://{path}"
             case _:
                 result = self.value
         if "." not in result:
