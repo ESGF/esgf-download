80b8dbdbe74a254d69c8d7a8ceec1a46c3828a7c
Sven Rodriguez <svenbmr@gmail.com>
2025-09-09 18:09:01 +0200

    move HintsDict to context.types

--- esgpull/context/__init__.py
+++ esgpull/context/__init__.py
@@ -1,12 +1,11 @@
 from esgpull.context.solr import (
-    HintsDict,
     ResultSearch,
     _distribute_hits_impl,
 )
 from esgpull.context.solr import (
     SolrContext as Context,
 )
-from esgpull.context.types import IndexNode
+from esgpull.context.types import HintsDict, IndexNode
 
 __all__ = [
     "Context",
