b994fbed36a7f66f09a96a7fe3f8093f6ec0619f
Sven Rodriguez <svenbmr@gmail.com>
2025-09-25 17:49:14 +0200

    move IndexNode to context/types.py

--- esgpull/context/__init__.py
+++ esgpull/context/__init__.py
@@ -1,12 +1,12 @@
 from esgpull.context.solr import (
     HintsDict,
-    IndexNode,
     ResultSearch,
     _distribute_hits_impl,
 )
 from esgpull.context.solr import (
     SolrContext as Context,
 )
+from esgpull.context.types import IndexNode
 
 __all__ = [
     "Context",
