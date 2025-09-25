4bdf6b3afa8bdb2dc5b87ad7692f3ad45e6591be
Sven Rodriguez <svenbmr@gmail.com>
2025-09-25 17:31:52 +0200

    move context.py to context/solr.py

---
+++ esgpull/context/__init__.py
@@ -0,0 +1,15 @@
+from esgpull.context.solr import (
+    Context,
+    HintsDict,
+    IndexNode,
+    ResultSearch,
+    _distribute_hits_impl,
+)
+
+__all__ = [
+    "Context",
+    "IndexNode",
+    "HintsDict",
+    "ResultSearch",
+    "_distribute_hits_impl",
+]
